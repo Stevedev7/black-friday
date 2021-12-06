4efb329e8b69d0246c27b4ef90e7de6a28de27e6
SteveDev7 <pinto_steve@yahoo.com>
2021-12-06 17:28:41 +0530

    Add GCS Paths to store blobs

--- components/copy-data/index.py
+++ components/copy-data/index.py
@@ -8,11 +8,22 @@ sample = read_csv('data/test.csv')
 parser = ArgumentParser()
 parser.add_argument('--dataset', type=str)
 parser.add_argument('--sample', type=str)
+parser.add_argument('--output-data-uri', type=str)
+parser.add_argument('--output-sample-uri', type=str)
 
 args = parser.parse_args()
 
+# Make paths to store datasets
+Path(args.output_data_uri).parent.mkdir(parents=True, exist_ok=True)
+Path(args.output_sample_uri).parent.mkdir(parents=True, exist_ok=True)
+
+
+# Make paths to store dataset path and write to a file
 Path(args.dataset).parent.mkdir(parents=True, exist_ok=True)
+Path(args.dataset).write_text(args.output_data_uri)
+
 Path(args.sample).parent.mkdir(parents=True, exist_ok=True)
+Path(args.sample).write_text(args.output_sample_uri)
 
 dataset.to_csv(args.dataset, index=False)
 sample.to_csv(args.sample, index=False)
--- index.py
+++ index.py
@@ -2,15 +2,19 @@ from kfp import dsl
 from kfp.components import func_to_container_op, load_component_from_file
 from kfp.compiler import Compiler
 
-@dsl.pipeline(name='v=black-friday-v2')
+@func_to_container_op
+def sample_output(output):
+    print(output)
+
+@dsl.pipeline(name='v=black-friday-sales-prediction')
 def pipeline():
     copy_data = load_component_from_file("components/copy-data/copy-data.yml")
-    clean_data = load_component_from_file("components/clean-data/clean-data.yml")
-    feature_scale = load_component_from_file("components/feature-engineering/feature-engineering.yml")
+    # clean_data = load_component_from_file("components/clean-data/clean-data.yml")
+    # feature_scale = load_component_from_file("components/feature-engineering/feature-engineering.yml")
 
     # component tasks
-    copy_data_task = copy_data()
-    clean_data_task = clean_data(copy_data_task.outputs["Dataset"])
-    feature_scale_task = feature_scale(clean_data_task.outputs["Data"])
+    copy_data_task = copy_data(output_data_uri="gs://black-friday-demo-bucket/demo/train/train.csv", output_sample_uri="gs://black-friday-demo-bucket/demo/sample/sample.csv")
+    # clean_data_task = clean_data(copy_data_task.outputs["Dataset"])
+    # feature_scale_task = feature_scale(clean_data_task.outputs["Data"])
 
-Compiler().compile(pipeline, 'black-friday-v2.yml')
+Compiler().compile(pipeline, 'test.yml')
