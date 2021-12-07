33a0728b650be017da7f9cc55d506c1bd7db2779
SteveDev7 <pinto_steve@yahoo.com>
2021-12-07 17:38:11 +0530

    Add Linear regression model training component

---
+++ components/linear-regression/index.py
@@ -0,0 +1,21 @@
+from pathlib import Path
+from pandas import read_csv
+from argparse import ArgumentParser
+from sklearn.linear_model import LinearRegression
+from pickle import dump
+
+parser = ArgumentParser()
+parser.add_argument('--x-train', type=str)
+parser.add_argument('--y-train', type=str)
+parser.add_argument('--model', type=str)
+args = parser.parse_args()
+
+x_train = read_csv(args.x_train)
+y_train = read_csv(args.y_train)
+
+linear_regressor = LinearRegression()
+linear_regressor.fit(x_train, y_train)
+
+Path(args.model).parent.mkdir(exist_ok=True, parents=True)
+with open(args.model, 'wb') as output_file:
+    dump(linear_regressor, output_file)
--- index.py
+++ index.py
@@ -7,10 +7,15 @@ def pipeline():
     copy_data = load_component_from_file("components/copy-data/copy-data.yml")
     clean_data = load_component_from_file("components/clean-data/clean-data.yml")
     feature_scale = load_component_from_file("components/feature-engineering/feature-engineering.yml")
+    linear_regression = load_component_from_file("components/linear-regression/linear-regression.yml")
+    # linear_regression_test = load_component_from_file("components/linear-regression-test/linear-regression-test.yml")
 
     # component tasks
     copy_data_task = copy_data()
     clean_data_task = clean_data(copy_data_task.outputs["Dataset"])
     feature_scale_task = feature_scale(clean_data_task.outputs["Data"])
+    linear_regression_task = linear_regression(feature_scale_task.outputs["X Train"], feature_scale_task.outputs["Y Train"])
+    # linear_regression_test_task = linear_regression_test(feature_scale_task.outputs["X Test"], feature_scale_task.outputs["Y Test"], linear_regression_task.outputs["Linear Regression Model"])
+    print(linear_regression_test_task.outputs)
 
 Compiler().compile(pipeline, 'black-friday-v2.yml')
